80a1d6abd21c79299aefcf0f44c9eac72e4f779c
Stefan Popp <mail@stefanpopp.de>
2015-07-10 23:51:46 +0200

    Initial commit, including screenshots, font and color theme

---
+++ install-retro.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# used from https://github.com/vinhnx/Ciapre-Xcode-theme/blob/master/install-ciapre-theme.sh
+
+destination="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes"
+
+if [[ ! -d $destination ]]; then
+    mkdir -p ${destination}
+fi
+
+find . -name "*.dvtcolortheme" -exec cp {} ${destination} \;
+
+echo "Copied themese to: ${destination}!"
